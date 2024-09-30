c05f9f8c476bc28d0d02328b2a7694c5874678a6
Rachel Burkhardt <r.burkhardt.ibclc@gmail.com>
2024-09-30 17:37:11 -0500

    Chapter 10 videos completed. Just checking how compare branches work

--- src/main/java/org/launchcode/hellospring/controllers/HelloController.java
+++ src/main/java/org/launchcode/hellospring/controllers/HelloController.java
@@ -3,53 +3,49 @@ package org.launchcode.hellospring.controllers;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
-
-@ResponseBody
 @Controller
+@ResponseBody
 @RequestMapping("hello")
 public class HelloController {
+    //handle request at path http://localhost:8080/hello
 
+//    @GetMapping("hello")
+//    @ResponseBody
+//    public String hello(){
+//        return "Hello, Spring!";
+//    }
 
-    //TODO: Handles request at the root path
-    @GetMapping
-    public String helloRoot() {
-        return "Hello, Spring";
-    }
-
-    //TODO: Handles get request at path/goodbye
+    //handle request at path //http://localhost:8080/hello/goodbye
     @GetMapping("goodbye")
-    public String goodbye() {
-        return "Goodbye, Spring";
+    public String goodbye(){
+        return "Goodbye, Spring!";
     }
 
-    //TODO: Responds to post requests at "/goodbye"   <---- also at /hello/goodbye
-    @PostMapping("goodbye")
-    public String goodbyePost() {
-        return "Post Goodbye";
-    }
-
-    //TODO: Handles request of the form /hello/LaunchCode
-    @GetMapping("{name}")
-    public String helloWithPathParam(@PathVariable String name) {
+    //handle requests of the form http://localhost:8080/hello?name=LaunchCode
+    @RequestMapping(method = {RequestMethod.GET, RequestMethod.POST})
+    public String helloWithQueryParam(@RequestParam String name){
         return "Hello, " + name + "!";
     }
 
-    //TODO: Create form that submits via post request     <--- lives at /hello/form
+    //handle requests of the form http://localhost:8080/hello/LaunchCode
+    @GetMapping("{name}")
+    public String helloWithPathParam(@PathVariable String name){
+        return "Hello, " + name + "!";
+    }
+
+    //handles requests for http://localhost:8080/hello/form
     @GetMapping("form")
     public String helloForm() {
         return "<html>" +
                 "<body>" +
-                "<form action='hello' method='post'>" +
-                "<input type='text' name='name'>" +
-                "<input type='submit' value= 'Greet me!'>" +
+                "<form action = '/hello' method = 'post'>" + // submit a request to /hello
+                "<input type = 'text' name = 'name' >" +
+                "<input type = 'submit' value = 'Greet Me!' >" +
                 "</form>" +
                 "</body>" +
                 "</html>";
     }
 
-    //TODO: Handler will do GET and POST request at the /hello path
-    @RequestMapping(method = {RequestMethod.GET, RequestMethod.POST})
-    public String helloWithQueryParam(@RequestParam String name) {
-        return "Hello " + name + "!";
-    }
+
+    //Is this what a change would be?
 }
