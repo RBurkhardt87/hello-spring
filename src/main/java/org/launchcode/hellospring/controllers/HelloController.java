54c56758861c4d1b46b1a978748c4c147ff3c29e
Rachel Burkhardt <r.burkhardt.ibclc@gmail.com>
2024-10-04 16:23:49 -0500

    chap 13 progress

--- src/main/java/org/launchcode/hellospring/controllers/HelloController.java
+++ src/main/java/org/launchcode/hellospring/controllers/HelloController.java
@@ -1,10 +1,14 @@
 package org.launchcode.hellospring.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @Controller
-@ResponseBody
+
 @RequestMapping("hello")
 public class HelloController {
     //handle request at path http://localhost:8080/hello
@@ -16,15 +20,32 @@ public class HelloController {
 //    }
 
     //handle request at path //http://localhost:8080/hello/goodbye
+    @ResponseBody
     @GetMapping("goodbye")
     public String goodbye() {
         return "Goodbye, Spring!";
     }
 
+
+
+
+
     //handle requests of the form http://localhost:8080/hello?name=LaunchCode
+
+    //NOTE: in the model.addAttribute(first, second);
+    //the first MUST match ${first} located in the html file
+    //the second MUST match the variable being declared in the handler
+    //EXAMPLE:  greeting matches ${greeting} in hello.html
+    //          theGreeting = "Hello," + name + "!"  matches theGreeting in the .addAttribute
+    //Method creates a String and adds that String as a variable to the model. Model is then passed into the view when we
+    //render the template by returning the template's name
+
+
     @RequestMapping(method = {RequestMethod.GET, RequestMethod.POST})
-    public String helloWithQueryParam(@RequestParam String name) {
-        return "Hello, " + name + "!";
+    public String helloWithQueryParam(@RequestParam String name, Model model) {
+        String theGreeting = "Hello," + name + " !";
+        model.addAttribute("greeting", theGreeting);
+        return "hello";
     }
 
     //handle requests of the form http://localhost:8080/hello/LaunchCode
@@ -34,40 +55,42 @@ public class HelloController {
 //    }
 
     //handles requests for http://localhost:8080/hello/form
+    //form is the name of our html thymeleaf file. Since we are returning a template we remove @ResponseBody
+    //Also, because there's no @ResponseBody, when spring boot sees us returning a String, it already assumes it is the
+    //name of a template, and then goes to render the template.
     @GetMapping("form")
     public String helloForm() {
-        return "<html>" +
-                "<body>" +
-                "<form action = 'form/{name}' method = 'POST'>" + // submit a request to /hello
-                "<input type = 'text' name = 'name' >" +
-                "<select name = 'language'>" +
-                "<option value= 'English'> English </option>" +
-                "<option value= 'Spanish'> Spanish </option>" +
-                "<option value= 'French'> French </option>" +
-                "<option value= 'German'> German </option>" +
-                "<option value= 'Swedish'> Swedish </option>" +
-                "<input type = 'submit' value = 'Greet Me!' >" +
-                "</form>" +
-                "</body>" +
-                "</html>";
+        return "form";
     }
 
     //Creating a post handler for form submission for name and language input
-    @PostMapping("form/{name}")
-    public static String createMessage(@RequestParam String name, String language) {
 
-        if (language.equals("Spanish")) {
-            return "Hola, " + name + "!";
-        } else if (language.equals("French")) {
-            return "Bonjour, " + name + "!";
-        } else if (language.equals("German")) {
-            return "Hallo, " + name + "!";
-        } else if (language.equals("Swedish")) {
-            return "Hej, " + name + "!";
-        } else {
-            return "Hello, " + name + "!";
-        }
+    @PostMapping("form/{name}")
+    public static String createMessage(@RequestParam String name, String language, Model model) {
+        String theGreeting = "Hello, " + name + " !";
+        model.addAttribute("greeting", theGreeting);
+        return "hello";
+//        if (language.equals("Spanish")) {
+//            return "Hola, " + name + "!";
+//        } else if (language.equals("French")) {
+//            return "Bonjour, " + name + "!";
+//        } else if (language.equals("German")) {
+//            return "Hallo, " + name + "!";
+//        } else if (language.equals("Swedish")) {
+//            return "Hej, " + name + "!";
+//        } else {
+//            return "Hello, " + name + "!";
+//        }
     }
 
+    @GetMapping("hello-names")
+    public String helloNames(Model model){
+        List<String> names = new ArrayList<>();
+        names.add("Rachel");
+        names.add("Miles");
+        names.add("Darryn");
+        model.addAttribute("names", names);
+        return "hello-list";
+    }
 
 }
